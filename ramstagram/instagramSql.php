5f0f2b59681e4f1dfd329b1611ba7bfcb432e276
Ahmed Mesusd <ahmedmesudb@gmail.com>
2015-05-22 12:26:19 +0300

    Revert "Revert "Yeni Dosyalar""
    This reverts commit 2dc8667afcabe2c2cbe55e0c2a4a593e874c8422.

---
+++ ramstagram/instagramSql.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+	$dsn = 'mysql:host=localhost;dbname=';
+	$user = '';
+	$password = '';
+
+try {
+    $db = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+	if($resimler = $db ->query('SELECT * FROM resimler')){
+
+	}else{
+		echo 'Sorguda bir hata meydana geldi.';
+		$error = $db->errorInfo();
+		echo 'Hata mesajı: ' . $error[2];
+	}
+
+	$link = $_POST["link"];
+	$query = $db->prepare('INSERT INTO resimler (link) VALUES(?)');
+	
+
+	$query->execute(array($link));
+
+	echo $link . " sql e eklendi";
+
+?>
